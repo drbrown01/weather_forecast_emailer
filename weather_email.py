ca123d740a1c43d57b0b6e215476df2edebb33ef
Dan <drbrown01@yahoo.com>
2017-05-31 21:25:00 -0500

    added try block and except block to print error if filenotfound

--- weather_email.py
+++ weather_email.py
@@ -1,10 +1,13 @@
 emails =[]
+try:
+    email_file = open('emails.txt', 'r')
 
-email_file = open('emails.txt', 'r')
+    for line in email_file:
+        
+        emails.append(line.strip())
 
-for line in email_file:
-    line = line.strip()
-    emails.append(line)
+except FileNotFoundError as err :
+    perint(err)
 
 print(emails)
     
