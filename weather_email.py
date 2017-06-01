642b36a5c7caf80247c77ca251a858d2558f85cf
Dan <drbrown01@yahoo.com>
2017-05-31 21:35:49 -0500

    changed list to dictionary, added tuple for email and name

--- weather_email.py
+++ weather_email.py
@@ -1,13 +1,13 @@
-emails =[]
+emails ={}
 try:
-    email_file = open('emails.txt', 'r')
+    email_file = open('emails2.txt', 'r')
 
     for line in email_file:
+        (email, name) = line.split(',')
+        emails[email] = name
         
-        emails.append(line.strip())
-
 except FileNotFoundError as err :
-    perint(err)
+    print(err)
 
 print(emails)
     
